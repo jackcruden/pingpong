86f486deac1a3df525484d2abeaa3d0dbef4a5dc
Jack Cruden <jackcruden@gmail.com>
2020-10-18 10:15:03 +0100

    Add profile pic to profile page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,6 +1,9 @@
 <x-app-layout>
     <div class="max-w-7xl mx-auto py-6 px-4 lg:px-8">
-        <div class="text-3xl">{{ $user->name }}</div>
+        <div class="flex mb-2 space-x-2">
+            <img src="{{ $user->profile_photo_url }}" class="rounded-full" width="40" height="40">
+            <div class="text-3xl">{{ $user->name }}</div>
+        </div>
 
         <div class="flex space-x-2">
             <div>🏆 {{ $user->games()->count() }} {{ \Illuminate\Support\Str::plural('game', $user->games()->count()) }} won</div>
